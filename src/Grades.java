815825eec21a935d5f6650d7f8a441a4951a8643
Derek Rodriguez <drod2169@gmail.com>
2015-03-29 07:34:17 -0400

    Add variables, ask how many questions, create an array

--- src/Grades.java
+++ src/Grades.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by derekrodriguez on 3/28/15.
  */
 public class Grades {
     public static void main(String[] args) {
+
+        int testQ; //number of questions
+        int mGrade;
+        int count = 0;
+        double total;
+        char yesno = 'y';
+
+
+        System.out.println("How many questions are there?");
+        Scanner scan = new Scanner(System.in);
+        testQ = scan.nextInt();
+        int[] testKey = new int[testQ];
 
     }
 }
